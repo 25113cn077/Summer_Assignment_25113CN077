3a07e79da6bb0d5112145f3489c4fba8a80e2152
prince gupta <prince@kali.prince>
2026-06-29 21:15:08 +0530

    Day-23 assignment is uploaded

---
+++ Day-23/Anagram.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine().toLowerCase();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine().toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Not Anagrams");
+            return;
+        }
+
+        char[] a = str1.toCharArray();
+        char[] b = str2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) {
+            System.out.println("Strings are Anagrams");
+        } else {
+            System.out.println("Strings are Not Anagrams");
+        }
+    }
+}
